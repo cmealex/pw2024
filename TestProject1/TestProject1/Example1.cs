9b18b93e94670f81047f8e65288d6f0647994ced
cmealex <alexsam20032003@yahoo.com>
2024-09-08 23:49:23 +0300

    add all examples

---
+++ TestProject1/TestProject1/Example1.cs
@@ -0,0 +1,382 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightTests;
+
+[TestFixture]
+public class TestNodeExes1 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+public class TestNodeExes2 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes3 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes4 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes10 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes5 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes6 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes7 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes8 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
+
+
+public class TestNodeExes9 : PlaywrightTest
+{
+    private IAPIRequestContext Request = null!;
+
+    [Test]
+    public async Task Test1()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        await Request.GetAsync("https://github.com/cmealex/pw2024.git");
+    }
+
+    [SetUp]
+    public async Task SetUpAPITesting()
+    {
+        await CreateAPIRequestContext();
+    }
+
+    private async Task CreateAPIRequestContext()
+    {
+        Request = await Playwright.APIRequest.NewContextAsync(new()
+        {
+        });
+    }
+
+    [TearDown]
+    public async Task TearDownAPITesting()
+    {
+        await Request.DisposeAsync();
+    }
+
+}
---
+++ TestProject1/TestProject1/Example2WithoutPWTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Playwright;
+
+namespace TestProject2
+{
+    public class Example2WithoutPWTests1
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests2
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests3
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests4
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests5
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests6
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests7
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests8
+    {
+        [Test]
+        public async Task Main1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+}
