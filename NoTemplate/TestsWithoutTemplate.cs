9ba9fbd81d923814cb4bc98c2102f820dfe86c49
cmealex <alexsam20032003@yahoo.com>
2024-09-09 00:16:21 +0300

    added another project withhout template

---
+++ NoTemplate/TestsWithoutTemplate.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace ProjectWithoutTemplate
+{
+    public class Example2WithoutPWTests1
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests12    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests2
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests3
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests4
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests5
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests6
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests7
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+
+    public class Example2WithoutPWTests8
+    {
+        [Test]
+        public async Task Test1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var request = await playwright.APIRequest.NewContextAsync();
+            await request.GetAsync("https://github.com/cmealex/pw2024.git");
+
+        }
+    }
+}
